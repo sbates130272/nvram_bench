088cf52017e0f44c8612a7d931261d75837112b1
Stephen Bates <sbates@raithlin.com>
2014-12-15 20:56:52 -0800

    Improvements in nvdimm.sh script

--- nvdimm.sh
+++ nvdimm.sh
@@ -1,33 +1,30 @@
 #!/bin/bash
 
-if [[ $# -neq 1 ]] ; then
-    echo 'nvdimm.sh requires one argument (a file on a mounted file system)'
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run as root or via sudo (preferred)" 1>&2
     exit 1
 fi
 
+if [[ $# -ne 1 ]] ; then
+    echo 'nvdimm.sh requires one argument (a file on a mounted file system)'
+    exit 1
+fi
 
 p=64
 while [ $p -lt 17000 ]
 do
 	echo "P = $p"
-	mkdir P$p
-	cd P$p
+	mkdir -p P$p
 	t=1
 	while [ $t -lt 17 ]
 	do
-		echo "	T = $t"
-		mkdir 't_'$t'_P_'$p
-		cd 't_'$t'_P_'$p
-			#for loop
-			for try in 1 2 3
-			do
-                    sudo nvram_bench -n 100 -t $t -p $p -rsl $1 > try_$try
+	    echo "	T = $t"
+	    mkdir -p P$p/'t_'$t'_P_'$p
+	    for try in 1 2 3
+	    do
+                ./build/nvram_bench -n 1 -f -t $t -p $p -rsl $1 > P$p/'t_'$t'_P_'$p/'try_'$try
             done
-            #end of for loop.
-
-		t=$[$t+1]
-		cd ..
+	    t=$[$t+1]
 	done
 	p=$[$p*2]
-	cd ..
 done
